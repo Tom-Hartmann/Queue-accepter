dd3d4667c05442f8fb1d4df6515627b6e5fe55e5
Sebastian Wernicke <megges14@gmail.com>
2023-03-05 09:31:08 +0100

    Added autoban & lock in
    Removed options.txt, swapped to config.json.
    Added autoban
    Added lock in preselected champ
    Updated Readme to be up to date.

--- README.md
+++ README.md
@@ -4,31 +4,42 @@
 
 Option to enable it urself manually or fully automatic for each game.
 
-![image](https://user-images.githubusercontent.com/35658558/213885042-942d8d4c-211f-4a84-9f7a-476392063271.png)
+![v1.0.0](https://user-images.githubusercontent.com/35658558/213885042-942d8d4c-211f-4a84-9f7a-476392063271.png)
+![v1.2.0](https://i.imgur.com/985tpzq.png)
 
 Already compiled file (the .exe) download link :
-- https://github.com/Tom-Hartmann/Queue-accepter/releases/tag/v1.0.0
+
+- [v1.0.0](https://github.com/Tom-Hartmann/Queue-accepter/releases/tag/v1.0.0)
+- [v1.2.0](https://github.com/Tom-Hartmann/Queue-accepter/releases/tag/v1.2.0)
 
 Features:
+
 - Click on the on/off button to beggin the search of a starting game or click on automode to let the program activate and deactivate the queue accepter.
 - The programm will automatically set to "off" once it found the game so no risk of clicking somewhere once you've started the game.
-- Automode will deaktivate once you are in Preparation and ingame, once you are back out it will reaktivate the Searching.
+- Automode will deactivate once you are in Preparation and ingame, once you are back out it will reactivate the searching.
 - If someone didn't accept the game the program will switch to "on" mode to search again
+- If you select Lock, it will autolock in the chamption you have pre selected.
+- If you select Ban, it will autoban the chamption named in the config.json file. Default beeing shaco. Note that this only works if you write the name correct! I will put most champs below so you can check without needing to go ingame.
 - That's it !
 
+Champions:
+```html
+Aatrox,Ahri,Akali,Akshan,Alistar,Amumu,Anivia,Annie,Aphelios,Ashe,Aurelion Sol,Azir,Bard,Bel'Veth,Blitzcrank,Brand,Braum,Caitlyn,Camille,Cassiopeia,Cho'Gath,Corki,Darius,Diana,Draven,Dr. Mundo,Ekko,Elise,Evelynn,Ezreal,Fiddlesticks,Fiora,Fizz,Galio,Gangplank,Garen,Gnar,Gragas,Graves,Gwen,Hecarim,Heimerdinger,Illaoi,Irelia,Ivern,Janna,Jarvan IV,Jax,Jayce,Jhin,Jinx,Kai'Sa,Kalista,Karma,Karthus,Kassadin,Katarina,Kayle,Kayn,Kennen,Kha'Zix,Kindred,Kled,Kog'Maw,LeBlanc,Lee Sin,Leona,Lillia,Lissandra,Lucian,Lulu,Lux,Malphite,Malzahar,Maokai,Master Yi,Miss Fortune,Mordekaiser,Morgana,Nami,Nasus,Nautilus,Neeko,Nidalee,Nilah,Nocturne,Nunu & Willump,Olaf,Orianna,Ornn,Pantheon,Poppy,Pyke,Qiyana,Quinn,Rakan,Rammus,Rek'Sai,Rell,Renata Glasc,Renekton,Rengar,Riven,Rumble,Ryze,Samira,Sejuani,Senna,Seraphine,Sett,Shaco,Shen,Shyvana,Singed,Sion,Sivir,Skarner,Sona,Soraka,Swain,Sylas,Syndra,Tahm Kench,Taliyah,Talon,Taric,Teemo,Thresh,Tristana,Trundle,Tryndamere,Twisted Fate,Twitch,Udyr,Urgot,Varus,Vayne,Veigar,Vel'Koz,Vex,Vi,Viego,Viktor,Vladimir,Volibear,Warwick,Wukong,Xayah,Xerath,Xin Zhao,Yasuo,Yone,Yorick,Yuumi,Zac,Zed,Zeri,Ziggs,Zilean,Zoe,Zyra
+```
 
+How to add your own language (English already included)  EXAMPLE:
 
-How to add your own language (English already included) :
 - Take a screenshot of the "accept" button of your game and cut it to only have the button using any image edit software (from paint to photoshop the choice is yours) and rename it "accept.png"
 - Do the same when someone doesn't accept the game (just take a part of the sentence like "declined ready check") and rename it "notaccept.png"
 - Copy the balance.png into your folder, it should be the same for all language, if it does not activate for you just take a new screenshot of the balances like shown in the picture.
 - The pictures don't have to be precise just be carefull you don't have any background showing and save them to .png
 - Create a new folder inside the "Languages" one (call it wathever you want but i reccomand using the language you're aiming to add) and put the two images you just cropped inside it.
-- Open the "Option.txt" file located in the "Languages" folder. Change the first line to "Language = [The name of the folder you just created]"
+- Open the "config.json" file located in the "Languages" folder. Change the first line to "Language = [The name of the folder you just created]"
 - This should work !
 
-How to compile the code into an .exe using cx_Freeze (https://cx-freeze.readthedocs.io/en/latest/index.html): 
-- Install cx_Freeze (by using pip : most likely by typing "python -m pip install --upgrade cx_Freeze" or "python3 -m pip install --upgrade cx_Freeze" on your command prompt) 
+How to compile the code into an .exe using cx_Freeze (https://cx-freeze.readthedocs.io/en/latest/index.html):
+
+- Install cx_Freeze (by using pip : most likely by typing "python -m pip install --upgrade cx_Freeze" or "python3 -m pip install --upgrade cx_Freeze" on your command prompt)
 - Open your command prompt and move to the directory where the script are
 - Type "python setup.py build" or "python3 setup.py build" depending on your version of python
 - The .exe should appear in "\build\exe.win-amd64-3.9\\" ("exe.win-amd64-3.9" will change according to your os and python version)
--- autoaccept.py
+++ autoaccept.py
@@ -2,47 +2,99 @@ from tkinter import *
 import os
 import time
 import pyautogui
+import json
 pyautogui.FAILSAFE = FALSE
-#Main function
+
+jsonpener = open('config.json', 'r').read()
+obj = json.loads(jsonpener)
+
+# Main function
+
+
 def img():
-    if toggle_button.config('text')[-1] == 'ON' :
-        print ("Searching for a game")
-        imglocation = pyautogui.locateCenterOnScreen('accept.png', confidence=0.7)
+    if toggle_button.config('text')[-1] == 'ON':
+        print("Searching for a game")
+        imglocation = pyautogui.locateCenterOnScreen(
+            'accept.png', confidence=0.7)
         if imglocation != None:
             toggle_button.config(text='OFF')
             CurrenPostion = pyautogui.position()
-            pyautogui.moveTo(imglocation,duration=0)
-            pyautogui.click()
-            pyautogui.moveTo(CurrenPostion,duration=0)
-            print ("gl")
+            # pyautogui.moveTo(imglocation,duration=0)
+            pyautogui.click(imglocation)
+            pyautogui.moveTo(CurrenPostion, duration=0)
+            print("gl")
+            # sleeping to not stay on while in champ select/ingame | tempfix
             time.sleep(10)
-            print ("Slept 10 seconds")
-    imglocation2 = pyautogui.locateCenterOnScreen('notaccept.png', confidence=0.7)
-    if imglocation2 != None :
+            print("Slept 10 seconds")
+    dodgelocation = pyautogui.locateCenterOnScreen(
+        'notaccept.png', confidence=0.7)
+    if dodgelocation != None:
         toggle_button.config(text='ON')
         print("Someone didn't accept!")
-    root.after(300,img)
-    imglocation3 = pyautogui.locateCenterOnScreen('balance.png', confidence=0.7)
-    if toggle_button.config('text')[-1] == 'OFF' and imglocation3 != None:
+    root.after(2000, img)
+    balancelocation = pyautogui.locateCenterOnScreen(
+        'balance.png', confidence=0.7)
+    if toggle_button.config('text')[-1] == 'OFF' and balancelocation != None:
         if automode_button.config('text')[-1] == 'Automode ON':
             toggle_button.config(text='ON')
-            print ('ON')
+            ban_button.config(text='BAN ON')
+            lockin_button.config(text='Lock ON')
+            print('ON')
+    banphase = pyautogui.locateCenterOnScreen('ban.png', confidence=0.9)
+    cancerchampbannen = pyautogui.locateCenterOnScreen(
+        'ban2.png', confidence=0.7)
+    searchbar = pyautogui.locateCenterOnScreen('search.png', confidence=0.9)
+    frame = pyautogui.locateCenterOnScreen('frame.png', confidence=0.7)
+    confirmban = pyautogui.locateCenterOnScreen(
+        'confirmban.png', confidence=0.7)  # Der bann muss durch!
+    if toggle_button.config('text')[-1] == 'OFF' and banphase != None:
+        if ban_button.config('text')[-1] == 'BAN ON':
+            print('BAN PHASE!')
+            pyautogui.click(searchbar)
+            # pyautogui.hotkey('ctrl','a')
+            # pyautogui.hotkey('return')
+            pyautogui.write(obj['banchamp'])
+            time.sleep(0.5)
+            print('Trying to click the frame')
+            print(frame)
+            xlst = list(frame)
+            del xlst[1]
+            lst = list(frame)
+            del lst[0]
+            print(xlst, lst)
+            # Thanks to gianpi#1307 für helping me with this part.
+            ycords = int(''.join(str(i) for i in lst))
+            ycords += 60
+            pyautogui.moveTo(frame)
+            x, y = pyautogui.position()
+            x, y = pyautogui.position()
+            pyautogui.click(y=ycords, x=x)
+            pyautogui.click(cancerchampbannen, duration=0)
+            time.sleep(2)
+            print('Checking for ban confirmation.')
+            pyautogui.click(confirmban, duration=0)
+            ban_button.config(text='BAN OFF')
+    lockinimg = pyautogui.locateCenterOnScreen('lockin.png', confidence=0.7)
+    if toggle_button.config('text')[-1] == 'OFF' and lockin_button != None:
+        if lockin_button.config('text')[-1] == 'Lock ON':
+            if lockinimg != None:
+                pyautogui.click(lockinimg, duration=0)
+                lockin_button.config(text='Lock OFF')
 
-#Get Language
+# Get Language
+
+
 def Lang():
     path = os.getcwd()
-    os.chdir(os.path.join(path,"Languages"))
+    os.chdir(os.path.join(path, "Languages"))
     path = os.getcwd()
 
-    f_in = open("Options.txt","r",encoding="utf-8")
-    text= f_in.readline()
-    f_in.close()
-    text=text[10:]
-    text=text.strip()
+    text = obj['Language']
+    os.chdir(os.path.join(path, text))
 
-    os.chdir(os.path.join(path,text))
-    
-#Button on/off
+# Button on/off
+
+
 def Simpletoggle():
     if toggle_button.config('text')[-1] == 'ON':
         toggle_button.config(text='OFF')
@@ -51,8 +103,10 @@ def Simpletoggle():
         toggle_button.config(text='ON')
         print("ON")
 
-#Button Automode on/off
-def SimpleToggle2():
+# Button Automode on/off
+
+
+def Autotoggle():
     if automode_button.config('text')[-1] == 'Automode ON':
         automode_button.config(text='Automode OFF')
         print('Automode OFF')
@@ -60,27 +114,65 @@ def SimpleToggle2():
         automode_button.config(text='Automode ON')
         print('Automode ON')
 
-#Window
+# Button Lock in on/off
+
+
+def Lockin():
+    if lockin_button.config('text')[-1] == 'Lock ON':
+        lockin_button.config(text='Lock OFF')
+        print('Lock OFF')
+    else:
+        lockin_button.config(text='Lock ON')
+        print('Lock ON')
+
+# Button Ban on/off
+
+
+def BanChamp():
+    if ban_button.config('text')[-1] == 'BAN ON':
+        ban_button.config(text='BAN OFF')
+        print('BAN OFF')
+    else:
+        ban_button.config(text='BAN ON')
+        print('BAN ON')
+
+
+# Window
 root = Tk()
 root.title("Queue Accepter")
 root.iconbitmap("icon.ico")
+root['background'] = 'black'
 Hauteur = 0
 Largeur = 250
-(H,L,c) = (Hauteur,Largeur,"white")
-Dessin = Canvas(root,height=H,width=L,bg=c)
+(H, L, c) = (Hauteur, Largeur, "white")
+Dessin = Canvas(root, height=H, width=L, bg=c)
 Dessin.pack()
 
-#Button
-toggle_button = Button(text='OFF', width=10, command=Simpletoggle)
+# Button
+toggle_button = Button(text='OFF', bg='black', fg='white',
+                       width=12, command=Simpletoggle)
 do = toggle_button['text']
-toggle_button.pack(pady=10)
+toggle_button.pack(pady=5)
 
-#Automode
-automode_button = Button(text='Automode off', width=12, command=SimpleToggle2)
+# Automode
+automode_button = Button(text='Automode off', bg='black',
+                         fg='white', width=12, command=Autotoggle)
 do = automode_button['text']
 automode_button.pack(pady=5)
 
+# Lock in
+lockin_button = Button(text='Lock OFF', bg='black',
+                       fg='white', width=12, command=Lockin)
+do = lockin_button['text']
+lockin_button.pack(pady=5)
+
+# BanChamp
+ban_button = Button(text='BAN OFF', bg='black',
+                    fg='white', width=12, command=BanChamp)
+do = ban_button['text']
+ban_button.pack(pady=5)
+
 Lang()
-#Loop
-root.after(2000,img)
+# Loop
+root.after(2000, img)
 root.mainloop()
