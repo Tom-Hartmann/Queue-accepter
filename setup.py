a9f1460d1c139dff0e0e55fd7cf981d43d0c5302
Sebastian Wernicke <megges14@gmail.com>
2023-04-22 13:14:35 +0200

    Update setup.py
    added files to builder

--- setup.py
+++ setup.py
@@ -4,19 +4,20 @@ from cx_Freeze import setup, Executable
 base = None
 if (sys.platform == "win32"):
     base = "Win32GUI"
- 
+
 executables = [Executable("autoaccept.py", base=base, icon="icon.ico")]
-packages = ["idna","tkinter","pyautogui","os"]
+packages = ["idna", "tkinter", "pyautogui", "os"]
 options = {
-    'build_exe': {    
-        'packages':packages,
+    'build_exe': {
+        'packages': packages,
+        'include_files': ['icon.ico', 'config.json', "Languages"]
     },
 }
 
 setup(
-    name = "autoaccept",
-    options = options,
-    version = "1.0",
-    description = 'Queue Accepter',
-    executables = executables
+    name="autoaccept",
+    options=options,
+    version="1.0",
+    description='Queue Accepter',
+    executables=executables
 )
